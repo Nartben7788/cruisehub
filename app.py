4be72583ab06ee39b2fc0f34e705fbb0f21093d6
Toluwanimi Sonuga <sonugatoluwanimi@gmail.com>
2024-01-16 09:23:27 +0100

    database

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cruisehub.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'yWNZU7s8'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    user_type = db.Column(db.String, nullable=False, default='user')  
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    user_type = db.Column(db.String, nullable=False, default='owner') 
+
+
+
+
+with app.app_context():
+    db.create_all()
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-if __name__ == ("__main__"):
+if __name__ == "__main__":
     app.run(debug=True)
