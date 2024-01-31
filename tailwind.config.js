31c8d6439842360ade4913c83c40a3f1df83bb49
samzyconcepts <samson.oderinwale@kibo.school>
2024-01-31 09:37:22 +0100

    styling the login screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./templates/**/*.html", "./static/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                blue: { DEFAULT: "#3652AD", light: "#E9F6FF", dark: "#280274" },
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "1rem",
+                    sm: "2rem",
+                    lg: "4rem",
+                    xl: "5rem",
+                    "2xl": "6rem",
+                },
+            },
+        },
     },
     plugins: [],
 };
