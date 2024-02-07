cafa99a9f81dfb6fc336f023a4a7c398d8b08b2b
Nartey Benjamin <benjamin.narteyteye@kibo.school>
2024-02-07 14:24:04 +0000

    Added more cars to seed2.py

--- seed2.py
+++ seed2.py
@@ -5,27 +5,117 @@ from datetime import datetime
 
 def seed_data():
     with app.app_context():
-        car7 = Car(
-            model='Corolla',
-            make ='Toyota',
-            price=300,
-            additional_info='open to negotiation',
-            picture='static/uploads/ethan_johnson/corolla.jpg',
-            owner_id= 4)
-            
-        
-        db.session.add(car7)
+        car_data = [
+            {
+                'model': 'Corolla',
+                'make': 'Toyota',
+                'price': 300,
+                'additional_info': 'open to negotiation',
+                'picture': 'static/uploads/ethan_johnson/corolla.jpg',
+                'owner_id': 4
+            },
+           
+            {
+                'model': 'Audi A4',
+                'make': 'Audi',
+                'price': 800,
+                'additional_info': 'fast on the road',
+                'picture': 'static/uploads/benny_johnson/audi.jpg',
+                'owner_id': 6
+            },
+            {
+                'model': 'Hyundai Sonata',
+                'make': 'Hyundai',
+                'price': 600,
+                'additional_info': 'affordable electric car',
+                'picture': 'static/uploads/helena_smith/hyundai.jpg',
+                'owner_id': 7
+            },
+            {
+                'model': 'Mercedes-Benz',
+                'make': 'Mercedes',
+                'price': 400,
+                'additional_info': 'Can roll!',
+                'picture': 'static/uploads/jackson_wilson/mercedes.jpg',
+                'owner_id': 8
+            },
+            {
+                'model': 'Subaru Outback',
+                'make': 'Subaru',
+                'price': 700,
+                'additional_info': 'sport car',
+                'picture': 'static/uploads/nimi/subaru.jpg',
+                'owner_id': 9
+            },
+            {
+                'model': 'Volvo XC90',
+                'make': 'Volvo',
+                'price': 300,
+                'additional_info': 'compact car',
+                'picture': 'static/uploads/benny_johnson/volvo.jpg',
+                'owner_id': 10
+            },
+            {
+                'model': 'BMW 7',
+                'make': 'BMW',
+                'price': 400,
+                'additional_info': 'comfortable car',
+                'picture': 'static/uploads/ethan_johnson/bmw7.jpg',
+                'owner_id': 4
+            },
+            {
+                'model': 'Jeep Wrangler',
+                'make': 'Jeep',
+                'price': 500,
+                'additional_info': 'Strong',
+                'picture': 'static/uploads/helena_smith/jeep.jpg',
+                'owner_id': 5
+            },
+            {
+                'model': 'Kia Sorento',
+                'make': 'Kia',
+                'price': 300,
+                'additional_info': 'compact car',
+                'picture': 'static/uploads/jackson_wilson/sorento.jpg',
+                'owner_id': 6
+            },
+            {
+                'model': 'Tesla Model S',
+                'make': 'Tesla',
+                'price': 600,
+                'additional_info': 'premium compact electric car',
+                'picture': 'static/uploads/nimi/tesla.jpg',
+                'owner_id': 7
+            },
+            {
+                'model': 'Altima',
+                'make': 'Nissan',
+                'price': 700,
+                'additional_info': 'premium compact SUV',
+                'picture': 'static/uploads/benny_johnson/nissan.jpg',
+                'owner_id': 8
+            },
+            {
+                'model': 'Ferrari 488',
+                'make': 'Ferrari',
+                'price': 500,
+                'additional_info': 'in excellent condition',
+                'picture': 'static/uploads/ethan_johnson/ferrari.jpg',
+                'owner_id': 5
+            },
+            {
+                'model': 'Challenger',
+                'make': 'Dodge',
+                'price': 400,
+                'additional_info': 'well maintained',
+                'picture': 'static/uploads/helena_smith/dodge.jpg',
+                'owner_id': 6
+            }
+        ]
+        for data in car_data:
+            car = Car(**data)
+            db.session.add(car)
         db.session.commit()
-       
-        
-        
-        
-        
-
-       
-        # # Add data to the database
-        # db.session.add_all([car1, car2,car3,car4, car5, car6,])
-        # db.session.commit()
 
 if __name__ == "__main__":
     seed_data()
